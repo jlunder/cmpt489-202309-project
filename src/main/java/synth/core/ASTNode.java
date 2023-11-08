ee40175695b0d8c8cb8894b900b8e51eec9c9df0
Joseph Lunderville <joseph_lunderville@sfu.ca>
2023-11-07 17:53:30 -0800

    Memoization doesn't help here

--- src/main/java/synth/core/ASTNode.java
+++ src/main/java/synth/core/ASTNode.java
@@ -27,35 +27,8 @@ public class ASTNode {
         return children.get(index);
     }
 
-    private record MakeParams(Symbol symbol, List<ASTNode> children) {
-    }
-
-    private static final HashMap<Symbol, WeakReference<ASTNode>> terminalsMemo = new HashMap<>();
-    private static final HashMap<MakeParams, WeakReference<ASTNode>> nonTerminalsMemo = new HashMap<>();
-
     public static ASTNode make(Symbol symbol, List<ASTNode> children) {
-        ASTNode node = null;
-        if (children == null || children.isEmpty()) {
-            var w = terminalsMemo.get(symbol);
-            if (w != null) {
-                node = w.get();
-            }
-            if (node == null) {
-                node = new ASTNode(symbol, List.of());
-                terminalsMemo.put(symbol, new WeakReference<ASTNode>(node));
-            }
-        } else {
-            var p = new MakeParams(symbol, children);
-            var w = nonTerminalsMemo.get(p);
-            if (w != null) {
-                node = w.get();
-            }
-            if (node == null) {
-                node = new ASTNode(symbol, children);
-                nonTerminalsMemo.put(p, new WeakReference<ASTNode>(node));
-            }
-        }
-        return node;
+        return new ASTNode(symbol, children);
     }
 
     @Override
